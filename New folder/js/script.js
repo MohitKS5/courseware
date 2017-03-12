f087ae6d90d31cc7154277f412f2cbb2113b8168
MohitKS5 <mohitkumarsingh907@gmail.com>
2017-03-12 18:03:22 +0530

    final dashboard animations

--- New folder/js/script.js
+++ New folder/js/script.js
@@ -1,31 +1,60 @@
+var count=0;
+/*---------------onscroll animation---------------------*/
 function transition(){
 	$(window).scroll(function(){
 		 var h = window.innerHeight;
-		 if (($(window).scrollTop() > 1) && ($(window).scrollTop() <10 )) {
-		 	anime_slide();
-		 	$('html, body').animate({
-                    scrollTop: $("#div1").offset().top
-                }, 2000);
+		
+		 if ($(window).scrollTop() <1) {count=0;
+		 }else if (($(window).scrollTop() > 1)&&(($(window).scrollTop() < 10))&&(count==0)) {
+		    anime_slide();
+		    count=10;
+		 	$('html, body').animate({scrollTop: $("#sec2").offset().top}, 1000);
+		 
+		 }else if ($(window).scrollTop() >h) {count=2;
+		 }else  if (($(window).scrollTop() > h-20)&&(($(window).scrollTop() < h+50))&&(count==2)) {
+		 	back_slide();
+		 	count=10;
+		 	$('html, body').animate({scrollTop: $("#sec1").offset().top},1000);
+
 		 }
+
+
+
 	});
+}
+
+
+function back_slide(){
+	$("#mainnav").animate({
+		width: "20vw",
+		opacity: 1,
+		}, 1500);
+	$("#framed").animate({
+		
+		width: "79vw",
+		opacity: 1,
+		}, 1500);
+
 }
 
 function anime_slide(){
 	$("#mainnav").animate({
 		width: "0px",
 		opacity: 1,
-		}, 1500 );
+		}, 1000);
 	$("#framed").animate({
+		
 		width: "0px",
 		opacity: 1,
-		}, 1500 );
+		}, 1000);
 
 }
+
 $(document).ready(function() {
 transition();
-$("#mynav").click(function() {
-alert("Hello world!");
-
+$("#top").click(function() {
+alert(count);});
+$("#div1").click(function() {
+alert(count);
 });
 });
-
